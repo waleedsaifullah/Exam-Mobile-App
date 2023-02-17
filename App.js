c9fd9c6958ce8b5afc770dd9bf54475d2b6fa40d
waleedsaifullah <waleedsaifullah786@gmail.com>
2023-02-17 23:13:28 +0500

    App Initialized

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+    return (
+        <View style={{
+            flex:1,
+            backgroundColor: 'purple',
+        }}>
+            <Text>ExamApp</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function SplashScreen() {
+    return (
+        <View>
+            <Text>SplashScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
